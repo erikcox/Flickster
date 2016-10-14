402532b6d04573b2cfd8851fa8fd672204e80635
Erik Cox <erikbcox@gmail.com>
2016-10-14 11:06:01 -0700

    Initial commit

---
+++ app/src/main/java/rocks/ecox/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,52 @@
+package rocks.ecox.flickster.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import rocks.ecox.flickster.R;
+import rocks.ecox.flickster.models.Movie;
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for position
+        Movie movie = getItem(position);
+
+        // Check the existing view being used
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // Find the ImageView
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // Clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // Populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        // Return the view
+        return convertView;
+    }
+}
