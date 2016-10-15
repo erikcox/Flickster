40f16d63716822cb7f7c2dcb8ffd280d6b47ea9c
Erik Cox <erikbcox@gmail.com>
2016-10-14 19:57:40 -0700

    Update style, dark theme

--- app/src/main/java/rocks/ecox/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/rocks/ecox/flickster/adapters/MovieArrayAdapter.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (c) 2016 Erik Cox
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package rocks.ecox.flickster.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +49,7 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
         }
 
+        convertView.setBackgroundColor(Color.BLACK);
         // Find the ImageView
         ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
         // Clear out image from convertView
